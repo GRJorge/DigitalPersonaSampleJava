c75701e4a7f5135d5fa021d3018e538f23a82b6d
axdevil <deb.devel@outlook.es>
2023-10-14 01:40:00 -0700

    Capture

---
+++ src/controller/capture.java
@@ -0,0 +1,79 @@
+package controller;
+
+import com.digitalpersona.onetouch.DPFPGlobal;
+import com.digitalpersona.onetouch.capture.DPFPCapture;
+import com.digitalpersona.onetouch.capture.DPFPCapturePriority;
+import com.digitalpersona.onetouch.capture.event.DPFPDataAdapter;
+import com.digitalpersona.onetouch.capture.event.DPFPDataEvent;
+import com.digitalpersona.onetouch.capture.event.DPFPReaderStatusEvent;
+import com.digitalpersona.onetouch.capture.event.DPFPReaderStatusListener;
+import com.digitalpersona.onetouch.capture.event.DPFPSensorEvent;
+import com.digitalpersona.onetouch.capture.event.DPFPSensorListener;
+import com.digitalpersona.onetouch.readers.DPFPReadersCollection;
+
+/**
+ *
+ * @author Jorge
+ */
+public class capture {
+    
+    private static final DPFPCapture capture = DPFPGlobal.getCaptureFactory().createCapture();
+    private static final DPFPReadersCollection readers = DPFPGlobal.getReadersFactory().getReaders();
+
+    //INICIALIZACION DE LA CAPTURA
+    public static void start() {
+        capture.setReaderSerialNumber(readers.get(0).getSerialNumber());
+        capture.setPriority(DPFPCapturePriority.CAPTURE_PRIORITY_LOW);
+        events();
+        capture.startCapture();
+    }
+
+    //SUSCRIPCION A EVENTOS DEL SENSOR
+    private static void events() {
+        capture.addDataListener(new DPFPDataAdapter() {
+            @Override
+            public void dataAcquired(final DPFPDataEvent e) {
+                msj("Huella capturada");
+            }
+        });
+
+        /*capture.addDataListener(new DPFPDataListener() {
+            @Override
+            public void dataAcquired(final DPFPDataEvent e) {
+                System.out.println("Captura de huella 2");
+            }
+        });*/
+        capture.addReaderStatusListener(new DPFPReaderStatusListener() {
+            @Override
+            public void readerConnected(DPFPReaderStatusEvent dpfprs) {
+                msj("Sensor conectado");
+            }
+
+            @Override
+            public void readerDisconnected(DPFPReaderStatusEvent dpfprs) {
+                msj("Sensor desconectado");
+            }
+        });
+
+        capture.addSensorListener(new DPFPSensorListener() {
+            @Override
+            public void fingerTouched(DPFPSensorEvent dpfpse) {
+                msj("El dedo se coloco en el sensor");
+            }
+
+            @Override
+            public void fingerGone(DPFPSensorEvent dpfpse) {
+                msj("El dedo se retiro del sensor");
+            }
+
+            @Override
+            public void imageAcquired(DPFPSensorEvent dpfpse) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        });
+    }
+
+    private static void msj(String msj) {
+        System.out.println(msj);
+    }
+}
--- src/controller/main.java
+++ src/controller/main.java
@@ -9,6 +9,6 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("hola");
+        capture.start();
     }
 }
