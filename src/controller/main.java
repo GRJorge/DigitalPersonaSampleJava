2257308e3f8015bf71ead2781c4c704ce86bcda9
axdevil <>
2023-10-14 01:32:37 -0700

    Iniciar proyecto Java

---
+++ src/controller/main.java
@@ -0,0 +1,14 @@
+package controller;
+
+/**
+ *
+ * @author Jorge
+ */
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hola");
+    }
+}
